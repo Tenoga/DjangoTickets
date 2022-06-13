1e891afe7f77230cd7704d8fee069acacf070531
Tenoga <castroherreradavid@gmail.com>
2022-06-13 11:59:25 -0500

    Final commit

--- coltrans/aplicaciones/pasajeros/views.py
+++ coltrans/aplicaciones/pasajeros/views.py
@@ -72,5 +72,5 @@ def pasajeros_delete(request, cod):
 
 # ----------Pasajeros Show-----------#
 def pasajeros_show(request, cod):
-    pasajero = Pasajero.objects.all()
-    return render(request, "pasajeros/pasajeros_show.html", {'pasajeros':pasajero})
+    pasajero = Pasajero.objects.get(id = cod)
+    return render(request, "pasajeros/pasajeros_show.html", {'pasajero':pasajero})
